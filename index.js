a0c7219d9f318d490657ee131a103c2a9480f021
Suphawida <66022983@up.ac.th>
2026-01-26 09:16:48 +0700

    init socket server

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("client connected:", socket.id);
+
+  socket.on("join-event", (eventId) => {
+    socket.join(eventId);
+  });
+});
+
+app.post("/emit", (req, res) => {
+  const { eventId, image } = req.body;
+  io.to(eventId).emit("new-image", image);
+  res.json({ ok: true });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log("Socket server running on", PORT);
+});
